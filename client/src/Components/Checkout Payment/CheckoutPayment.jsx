204261db4f71a7a56a6a6965df68a0a7605c19f8
mohammedniyaf <mohammedniyaf77@gmail.com>
2024-11-14 10:08:39 +0530

    added  razorpay

--- client/src/Components/Checkout Payment/CheckoutPayment.jsx
+++ client/src/Components/Checkout Payment/CheckoutPayment.jsx
@@ -56,8 +56,6 @@ function CheckoutPayment() {
                     `${import.meta.env.VITE_BACKEND_URL}/api/user/order/create`,
                     {
                         totalAmount: cartTotalAmount,
-                        address: selectedAddress,
-                        paymentMethod: 'RazorPay'
                     },
                     { headers: { Authorization: `Bearer ${token}` } }
                 );
