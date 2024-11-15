dc0523fe925a5b8afc1e5cccb3b11972cac6f655
mohammedniyaf <mohammedniyaf77@gmail.com>
2024-11-15 20:53:33 +0530

    added image to order schema

--- server/MODEL/orderSchema.js
+++ server/MODEL/orderSchema.js
@@ -12,6 +12,7 @@ const orderSchema = new Schema({
         PrdId:{ type: String, required: true },
         name: { type: String, required: true },
         category: { type: String, required: true },
+        images:{ type: String, required: true },
         subCategory: { type: String, required: true },
         price: { type: Number, required: true },
         description: { type: String, required: true },
